deff70855a60e47705e5f479e89765b55978c51d
ISFlores <isaacfloresv@gmail.com>
2022-07-28 16:47:09 +0000

    agregados los models

--- src/app.py
+++ src/app.py
@@ -1,16 +1,25 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
+import json
 
-todos = { "label": "My second task", "done": False }
+todos = [ { "label": "My first task", "done": False }, ]
+some_data = [ { "label": "My secund task", "done": False } ]
 
 app = Flask(__name__)
 
-@app.route('/blabla',methods=['GET'])
+
+@app.route('/todos', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    # you can convert that variable into a json string like this
+    json_text = jsonify(todos)
 
-@app.route('/todos',methods=['GET'])
-def hello():
-    return jsonify(todos), 400
-
+    # and then you can return it to the front end in the response body like this
+    return json_text, 400
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = json.loads(request.data)
+    request_body = jsonify(request.data)
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
+    
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=3245,debug=True)
